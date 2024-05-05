2afcfb0bb82e6d433e6dc51193ada60a55f78dd3
NavaCorp26 <luisnavarrete1a89@gmail.com>
2024-05-05 13:39:18 -0500

    Alpha

---
+++ Project1/Models/Class.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Models
+{
+    public class Class
+    {
+        private SqlConnection conexion = new SqlConnection("Data Source=NAVARRETE\\SQLEXPRESS;Initial Catalog=Proyecto_DAWA;Integrated Security=True");
+        public SqlConnection OpenConnection()
+        {
+            if (conexion.State == ConnectionState.Closed)
+                conexion.Open();
+            return conexion;
+        }
+        public SqlConnection CloseConnection()
+        {
+            if (conexion.State == ConnectionState.Open)
+                conexion.Close();
+            return conexion;
+        }
+        //Coneccion actual
+        public SqlConnection GetCurrentConnection()
+        {
+            return conexion;
+        }
+    }
+}
